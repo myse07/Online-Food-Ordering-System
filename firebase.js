402b1b0a3b057e6e9d413621e0554fe8861fea8b
AsiCalumpad <acecalumpad18@gmail.com>
2026-05-18 00:05:24 +0800

    Save local changes before pushing to GitHub

--- firebase.js
+++ firebase.js
@@ -1,22 +1,15 @@
-// firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCV5fqc29jcNFDJRmTA-mIwWJChAB6iGjQ",
-  authDomain: "ordering-system-5e268.firebaseapp.com",
-  projectId: "ordering-system-5e268",
-  storageBucket: "ordering-system-5e268.firebasestorage.app",
-  messagingSenderId: "1085705731990",
-  appId: "1:1085705731990:web:2e36ad5d4e9019fb5867b7"
+  apiKey: "AIzaSyC8W3iJL2Y3JA2rvOzecLsssEMby7ZHPsQ",
+  authDomain: "online-ordering-system-7a05f.firebaseapp.com",
+  projectId: "online-ordering-system-7a05f",
+  storageBucket: "online-ordering-system-7a05f.firebasestorage.app",
+  messagingSenderId: "1029089563464",
+  appId: "1:1029089563464:web:0399698fc4103e9312d97d"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
-const db = getFirestore(app);
-
-// Export Firestore database
-export { db };
+export const db = getFirestore(app);
